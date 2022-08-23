832c72d63b6544efb5d047aeaacb456b74629d2c
Sazix1337 <sazix1337@gmail.com>
2022-08-23 13:09:18 +0300

    Update: All variables must be in quotes, and you can use string type now

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class Messx {
         const root = parse(fs.readFileSync(rootDir));
         const variables = [];
         const VarError = require('./varError');
+        const rimraf = require('rimraf');
 
         function variablesCheck() {
             for(let i = 0; i < variables.length; i++) {
@@ -18,12 +19,12 @@ class Messx {
         if(root.innerHTML.search(/\{\{.*?\}\}+/gim) !== -1) {
             const msxs = root.innerHTML.match(/\{\{.*?\}\}+/gim);
             msxs.forEach(msx => {
-                const variableNode = msx.split(/[\{\} ]/gim).filter(s => s != '').join('');
+                const variableNode = msx.split(/[\{\}]/gim).filter(s => s != '').join('').split(/^ +/gim).filter(s => s != '').join('');
                 if(variableNode.charAt(0) == "%") {
                     const variable = variableNode.split(/[\%\(\)]+/gim).filter(s => s != '').join('');
                     if(variable.match('=')) {
                         const leftOperand = variable.split('=')[0];
-                        const rightOperand = variable.split('=')[1];
+                        const rightOperand = variable.split('=')[1].split('"').join('');
                         variables.push({
                             variableName: leftOperand,
                             variableValue: rightOperand
@@ -40,13 +41,13 @@ class Messx {
                 msxs.forEach(msx => {
                     let variableNode = msx.split(/[\{\}\$ ]/gim).filter(s => s != '').join('');
                     if(variableNode.charAt(0) == "%") return;
-            
-                    const variable = variables.filter(n => n.variableName == variableNode);
-                    variableNode = variable[0].variableValue;
-                    root.innerHTML = root.innerHTML.replace(`${variable[0].variableName}$`, variableNode);
-                    const data = root.innerHTML.replaceAll('{', "").replaceAll('}', "").split(/\%\(.*?\)/gim).join('').split(/ \= \d+/gim).join('');
+                    const variable = variables.filter(n => n.variableName.split(' ').join('') == variableNode);
+                    variableNode = variable[0].variableValue.split(/^ +/gim).join('').split(/ $/gim).filter(s => s != '').join('');
+                    root.innerHTML = root.innerHTML.replace(`${variable[0].variableName.split(/ $/gim).join('')}$`, variableNode);
+                    const data = root.innerHTML.replaceAll('{', "").replaceAll('}', "").split(/\%\(.*?\)/gim).join('').split(/\= \".*?\"+/gim).join('');
+                    console.log(data);
                     fs.mkdir(path.join(__dirname, 'build'), error => {
-                        if(error) {};
+                        if(error) {}
 
                         fs.writeFile(path.join(__dirname, 'build', 'public.html'), data, error => {
                             if(error) {};
